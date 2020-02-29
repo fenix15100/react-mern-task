82e20fbcb33de29ce3862ea896963933973e0db2
fenix15100 <francisco.camacho15@gmail.com>
2020-02-29 08:55:03 +0100

    Initial commit

--- client/src/components/auth/SignUp.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SignUp = () => {
-    return (
-        <h1>newAccount</h1>
-    );
-}
-
-export default SignUp;
--- client/src/components/projects/Projects.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-const Projects = () => {
-    return (
-        <h1>Projectos</h1>
-    );
-}
-
-export default Projects;
