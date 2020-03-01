954e18c4e354c48de0ffe54b860c4809e34aa1e4
fenix15100 <francisco.camacho15@gmail.com>
2020-03-01 08:11:44 +0100

    prove proxy client

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect, useCallback } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const data = useCallback(async()=>{
+    let response = await fetch(`/api/ping`);
+    let data = await response.json()
+    console.log(data)
+
+  },[])
+
+  useEffect(()=>{
+    data()
+    
+  },[data])
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload
         </p>
         <a
           className="App-link"
--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const app = express();
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+app.get('/api/ping', function (req, res) {
+    res.set('Content-Type', 'application/json');
+    res.send('{"message":"Hello from the custom server!"}');
+  });
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
